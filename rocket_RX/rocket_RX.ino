8294a4b864c35557363c63407002ea0670cbcb4e
Jack Gordon <appjackstudio@gmail.com>
2018-08-31 13:08:41 -0500

    Seems to work, need to clean up code

--- rocket_RX/rocket_RX.ino
+++ rocket_RX/rocket_RX.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <RH_RF95.h>
+#include <ArduinoJson.h>
 
 #define RFM95_CS 8
 #define RFM95_RST 4
@@ -16,60 +17,69 @@ void setup()
   digitalWrite(RFM95_RST, HIGH);
 
   Serial.begin(115200);
-  
-  delay(100);
+  while(!Serial){}
 
-  Serial.println("Feather LoRa RX Test!");
+  // Serial.println("Feather LoRa TX Test!");
 
   // manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
-
-  while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
-    while (1);
-  }
-  Serial.println("LoRa radio init OK!");
+  rf95.init();
+  // Serial.println("LoRa radio init OK!");
 
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
-    while (1);
-  }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  rf95.setFrequency(RF95_FREQ);
 
+  // Serial.print("Set Freq to: ");Serial.println(RF95_FREQ);
+  
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
   // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
 }
+
+struct GPSFix {
+  long timestamp;
+  float latitude;
+  float longitude;
+  float altitude;
+  uint8_t fixQuality;
+  uint8_t numSats;
+};
+GPSFix fix;
 
 void loop()
 {
   if (rf95.available())
   {
     // Should be a message for us now
-    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    uint8_t len = sizeof(buf);
-
-    if (rf95.recv(buf, &len))
+    // uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(GPSFix);
+    if (rf95.recv((char*)&fix, &len))
     {
       digitalWrite(LED_BUILTIN, HIGH);
-      RH_RF95::printBuffer("Received: ", buf, len);
-      Serial.print("Got: ");
-      Serial.println((char*)buf);
-       Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
+      // RH_RF95::printBuffer("Received: ", (char*)&fix, len);
+      // Serial.print("Got: ");
+      // Serial.println((char*)buf);
+      // Serial.println(fix.toString());
+      // Serial.print("RSSI: ");
+      // Serial.println(rf95.lastRssi(), DEC);
+      StaticJsonBuffer<200> jsonBuffer;
+      JsonObject& root = jsonBuffer.createObject();
+      root["timestamp"] = fix.timestamp;
+      root["latitude"] = fix.latitude;
+      root["longitude"] = fix.longitude;
+      root["altitude"] = fix.altitude;
+      root["fixQuality"] = fix.fixQuality;
+      root["numSats"] = fix.numSats;
 
-      // Send a reply
-      uint8_t data[] = "And hello back to you";
-      rf95.send(data, sizeof(data));
-      rf95.waitPacketSent();
-      Serial.println("Sent a reply");
+      root.printTo(Serial);
+      // Serial.println();
+
       digitalWrite(LED_BUILTIN, LOW);
     }
     else
--- rocket_TX/rocket_TX.ino
+++ rocket_TX/rocket_TX.ino
@@ -9,13 +9,16 @@
 
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
+
 void setup() 
 {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
-  Serial.begin(115200); 
+  Serial.begin(115200);
+  // while(!Serial){}
+  Serial1.begin(9600);
 
   delay(100);
 
@@ -26,19 +29,13 @@ void setup()
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
-
-  while (!rf95.init()) {
-    Serial.println("LoRa radio init failed");
-    while (1);
-  }
+  rf95.init();
   Serial.println("LoRa radio init OK!");
 
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-  if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("setFrequency failed");
-    while (1);
-  }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  rf95.setFrequency(RF95_FREQ);
+
+  Serial.print("Set Freq to: ");Serial.println(RF95_FREQ);
   
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
@@ -48,49 +45,125 @@ void setup()
   rf95.setTxPower(23, false);
 }
 
-int16_t packetnum = 0;  // packet counter, we increment per xmission
+bool newGPSData;
+
+struct GPSFix {
+  long timestamp;
+  float latitude;
+  float longitude;
+  float altitude;
+  uint8_t fixQuality;
+  uint8_t numSats;
+  String toString(){
+    return String(timestamp) + '\n' +
+    String(latitude) + ", " +
+    String(longitude) + ", " +
+    String(altitude) + '\n' +
+    "Fix Quality " + String(fixQuality) + '\n' +
+    String(numSats) + " Satalites";
+  }
+};
+GPSFix fix;
 
 void loop()
 {
-  delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
-  Serial.println("Transmitting..."); // Send a message to rf95_server
-  
-  char radiopacket[20] = "Hello World #      ";
-  itoa(packetnum++, radiopacket+13, 10);
-  Serial.print("Sending "); Serial.println(radiopacket);
-  radiopacket[19] = 0;
-  
-  Serial.println("Sending...");
-  delay(10);
-  rf95.send((uint8_t *)radiopacket, 20);
+  updateGPS();
+  if(newGPSData){
+    newGPSData = false;
+    Serial.println();
+    Serial.println(fix.toString());
+    Serial.println();
+    char* raw = (char*)&fix;
+    rf95.send(raw, sizeof(GPSFix));
+    rf95.waitPacketSent();
+  }
+  // rf95.send((uint8_t *)radiopacket, 20);
+  // rf95.waitPacketSent();
+  // if (rf95.waitAvailableTimeout(1000))
+  //   if (rf95.recv(buf, &len))
+}
 
-  Serial.println("Waiting for packet to complete..."); 
-  delay(10);
-  rf95.waitPacketSent();
-  // Now wait for a reply
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
 
-  Serial.println("Waiting for reply...");
-  if (rf95.waitAvailableTimeout(1000))
-  { 
-    // Should be a reply message for us now   
-    if (rf95.recv(buf, &len))
-   {
-      Serial.print("Got reply: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);    
+String GPSBuffer = "";
+
+void updateGPS(){
+  while(Serial1.available()){
+    char c = Serial1.read();
+    // Serial.print("Buff: ");
+    // Serial.println(GPSBuffer);
+    if (c == '$') {
+      // Process Command
+      processCommand();
+      // Clear Buffer
+      GPSBuffer = "";
     }
-    else
-    {
-      Serial.println("Receive failed");
+    GPSBuffer += c;
+  }
+}
+
+void processCommand(){
+  // Only process GGA
+  // http://aprs.gids.nl/nmea/#gga
+  if(GPSBuffer.startsWith("$GPGGA")){
+    unsigned int len = GPSBuffer.length();
+    String command = GPSBuffer.substring(1, len - 5);
+    String csstr = GPSBuffer.substring(len - 4, len - 2);
+
+    Serial.println(command);
+    int clen = command.length();
+    byte cs1 = 0x00;
+    for(int i = 0; i<clen; i++){
+      cs1 ^= command[i];
+    }
+    byte cs2 = parseByte(csstr);
+    bool cs = (cs1 == cs2);
+
+    if(cs){
+      // Parsing Command
+      newGPSData = true;
+      parseGGA(command);
+    }else{
+      Serial.print("    Warning: checksum incorrect: 0x");
+      Serial.print(cs1, HEX);
+      Serial.print(" 0x");
+      Serial.print(cs2, HEX);
+      Serial.print(" 0x");
+      Serial.println(csstr);
     }
   }
-  else
-  {
-    Serial.println("No reply, is there a listener around?");
-  }
+}
+
+void parseGGA(String command){
+
+    // Find Commas
+    int indecies[11];
+    indecies[0] = 5;
+    for(int i = 1; i<10; i++){
+      indecies[i] = command.indexOf(',', indecies[i - 1] + 1);
+    }
+
+
+
+    // Extract data
+    fix.timestamp = command.substring(indecies[0]+1, indecies[1]).toFloat();
+  
+    String _lat = command.substring(indecies[1]+1, indecies[2]);
+    char _ns = command[indecies[2]+1];
+    fix.latitude = _lat.toFloat() * ((_ns == 'N') ? 1.0 : -1.0);
+
+    String _lon = command.substring(indecies[3]+1, indecies[4]);
+    char _ew = command[indecies[4]+1];
+    fix.longitude = _lon.toFloat() * ((_ew == 'E') ? 1.0 : -1.0);
+
+    fix.fixQuality = command.substring(indecies[5]+1, indecies[6]).toInt();
+    fix.numSats = command.substring(indecies[6]+1, indecies[7]).toInt();
+
+    fix.altitude = command.substring(indecies[8]+1, indecies[9]).toFloat();
 
 }
 
+byte parseByte(String hexValue){
+  byte tens = (hexValue[0] <= '9') ? hexValue[0] - '0' : hexValue[0] - '7';
+  byte ones = (hexValue[1] <= '9') ? hexValue[1] - '0' : hexValue[1] - '7';
+  return (16 * tens) + ones;
+}
